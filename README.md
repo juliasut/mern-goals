a8e0db9a1325eff3aa27122c368c8ab6e7ffe1fd
juliasut <juliasutygina@gmail.com>
2023-01-13 15:13:48 -0700

    define all CRUD routes for /api/goals

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ## 1 REST API with Express
+
+Separate routes for /api/goals under goalsRouter
--- backend/routes/goalsRouter.js
+++ backend/routes/goalsRouter.js
@@ -5,4 +5,16 @@ goalsRouter.get('/', (req, res) => {
   return res.status(200).json({ message: 'Get goals' });
 });
 
+goalsRouter.post('/', (req, res) => {
+  return res.status(201).json({ message: 'Set goals' });
+});
+
+goalsRouter.put('/:id', (req, res) => {
+  return res.status(200).json({ message: `Update goal ${req.params.id}` });
+});
+
+goalsRouter.delete('/:id', (req, res) => {
+  return res.status(200).json({ message: `Delete goal ${req.params.id}` });
+});
+
 module.exports = goalsRouter;
