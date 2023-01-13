ffdd07d30cb8d3b39bc090d50c516a24373dd9a6
juliasut <juliasutygina@gmail.com>
2023-01-13 16:25:28 -0700

    use error handling middleware

--- backend/controllers/goalsController.js
+++ backend/controllers/goalsController.js
@@ -9,6 +9,10 @@ const getGoals = (req, res) => {
 // @route POST /api/goals
 // @access Private
 const setGoal = (req, res) => {
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error('Please add a text field');
+  }
   return res.status(201).json({ message: 'Set new goal' });
 };
 
@@ -26,7 +30,7 @@ const deleteGoal = (req, res) => {
   return res.status(200).json({ message: `Delete goal ${req.params.id}` });
 };
 
-module.export = {
+module.exports = {
   getGoals,
   setGoal,
   updateGoal,
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const { errorHandler } = require('./middleware/errorMiddleware');
 const dotenv = require('dotenv').config();
 
 const goalsRouter = require('./routes/goalsRouter');
+const errorHandler = require('./middleware/errorMiddleware');
 const PORT = process.env.PORT || 8000;
 
 const app = express();
+
+app.use(express.json());
+
+/* 
+The extended option allows to choose between parsing the URL-encoded data with the querystring library (when false) or the qs library (when true). The "extended" syntax allows for rich objects and arrays to be encoded into the URL-encoded format, allowing for a JSON-like experience with URL-encoded.
+*/
+app.use(express.urlencoded({ extended: false }));
+app.use(errorHandler);
 
 app.use('/api/goals', goalsRouter);
 
