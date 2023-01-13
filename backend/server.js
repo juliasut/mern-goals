b79bac7c93e6fcf4278ea8eb1e4d2a11240f9af9
juliasut <juliasutygina@gmail.com>
2023-01-13 16:31:26 -0700

    fix error handler to correct position for app.use chain

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,6 @@ const { errorHandler } = require('./middleware/errorMiddleware');
 const dotenv = require('dotenv').config();
 
 const goalsRouter = require('./routes/goalsRouter');
-const errorHandler = require('./middleware/errorMiddleware');
 const PORT = process.env.PORT || 8000;
 
 const app = express();
@@ -14,9 +13,10 @@ app.use(express.json());
 The extended option allows to choose between parsing the URL-encoded data with the querystring library (when false) or the qs library (when true). The "extended" syntax allows for rich objects and arrays to be encoded into the URL-encoded format, allowing for a JSON-like experience with URL-encoded.
 */
 app.use(express.urlencoded({ extended: false }));
-app.use(errorHandler);
 
 app.use('/api/goals', goalsRouter);
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
