5d5e242eaeea736a2320cbb1fe444ae016fbe95f
juliasut <juliasutygina@gmail.com>
2023-01-13 18:00:29 -0700

    connect to mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const colors = require('colors');
 const { errorHandler } = require('./middleware/errorMiddleware');
 const dotenv = require('dotenv').config();
-
+const connectDB = require('./config/db');
 const goalsRouter = require('./routes/goalsRouter');
 const PORT = process.env.PORT || 8000;
+
+connectDB();
 
 const app = express();
 
