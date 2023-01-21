76ec0e1a4efe5f937a963f158f13f66087a4d4da
juliasut <juliasutygina@gmail.com>
2023-01-20 17:13:20 -0700

    use auth middleware with bcrypt and jwt

--- backend/routes/usersRouter.js
+++ backend/routes/usersRouter.js
@@ -5,9 +5,10 @@ const {
   loginUser,
   getMe,
 } = require('../controllers/usersController');
+const {protect} = require('../middleware/authMiddleware')
 
 usersRouter.post('/', registerUser);
 usersRouter.post('/login', loginUser);
-usersRouter.get('/me', getMe);
+usersRouter.get('/me', protect, getMe);
 
 module.exports = usersRouter;
